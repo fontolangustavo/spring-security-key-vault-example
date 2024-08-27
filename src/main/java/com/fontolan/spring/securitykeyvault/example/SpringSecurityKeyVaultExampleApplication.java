5901843bebb5a6501af2df0a134bbf7152d05c36
Fontolan <fontolangustavo@gmail.com>
2024-08-27 18:24:58 -0300

    chore: init project

---
+++ src/main/java/com/fontolan/spring/securitykeyvault/example/SpringSecurityKeyVaultExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fontolan.spring.securitykeyvault.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityKeyVaultExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityKeyVaultExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fontolan/spring/securitykeyvault/example/SpringSecurityKeyVaultExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontolan.spring.securitykeyvault.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityKeyVaultExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
